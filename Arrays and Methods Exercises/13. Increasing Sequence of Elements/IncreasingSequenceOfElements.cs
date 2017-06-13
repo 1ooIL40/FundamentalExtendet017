0780ec38d7eced426f8b3e942f7b0cb6170bea93
1ooiL40 <valkovstoil@abv.bg>
2017-06-13 16:57:04 +0300

    Increasing Sequence of Elements

---
+++ Arrays and Methods Exercises/13. Increasing Sequence of Elements/IncreasingSequenceOfElements.cs
@@ -0,0 +1,33 @@
+﻿namespace _13.Increasing_Sequence_of_Elements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class IncreasingSequenceOfElements
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var answer = string.Empty;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+
+                answer = "Yes";
+                if (numbers[i] > numbers[i + 1])
+                {
+                    answer = "No";
+                    break;
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
