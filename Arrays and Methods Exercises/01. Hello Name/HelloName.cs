81b35b049ee4064a9b2a2e09dc3ef44a7c2bcd73
1ooiL40 <valkovstoil@abv.bg>
2017-06-13 14:23:06 +0300

    Hello Name

---
+++ Arrays and Methods Exercises/01. Hello Name/HelloName.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Hello_Name
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HelloName
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
