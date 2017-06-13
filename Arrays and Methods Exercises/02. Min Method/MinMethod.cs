0910bdb94bdfac606321b70a21581e81a2739e13
1ooiL40 <valkovstoil@abv.bg>
2017-06-13 14:29:51 +0300

    Min Method

---
+++ Arrays and Methods Exercises/02. Min Method/MinMethod.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.Min_Method
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MinMethod
+    {
+        public static void Main()
+        {
+            var numOne = int.Parse(Console.ReadLine());
+            var numTwo = int.Parse(Console.ReadLine());
+            var numThree = int.Parse(Console.ReadLine());
+
+            var result = GetMin(numThree, GetMin(numOne, numTwo));
+            Console.WriteLine(result);
+        }
+
+        static int GetMin(int numOne, int numTwo)
+        {
+            if(numOne > numTwo)
+            {
+                return numTwo;
+            }
+            return numOne;
+        }
+    }
+}
