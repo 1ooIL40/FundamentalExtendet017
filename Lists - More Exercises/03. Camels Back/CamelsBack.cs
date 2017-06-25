e5bc91f70eba96fbebab38030bd57f8618b91944
1ooiL40 <valkovstoil@abv.bg>
2017-06-25 15:28:13 +0300

    Camels Back

---
+++ Lists - More Exercises/03. Camels Back/CamelsBack.cs
@@ -0,0 +1,38 @@
+﻿namespace _03.Camels_Back
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CamelsBack
+    {
+        public static void Main()
+        {
+            var buildings = Console.ReadLine()
+                .Split(new char[] { ' ' })
+                .Select(int.Parse)
+                .ToList();
+
+            var camelsBackSize = int.Parse(Console.ReadLine());
+
+
+            if(buildings.Count <= camelsBackSize)
+            {
+                Console.WriteLine($"already stable: {string.Join(" ",buildings)}");
+            }
+            else
+            {
+                var rouds = (buildings.Count - camelsBackSize) / 2;
+
+
+                buildings = buildings
+                    .Skip(rouds)
+                    .Take(camelsBackSize)
+                    .ToList();
+
+                Console.WriteLine($"{rouds} rounds");
+                Console.WriteLine($"remaining: {string.Join(" ", buildings)}");
+            }
+        }
+    }
+}
