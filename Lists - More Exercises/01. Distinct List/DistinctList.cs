5098c13538acd8cd4d0323b0c2cc0fce145cb97f
1ooiL40 <valkovstoil@abv.bg>
2017-06-25 15:09:46 +0300

    Distinct Lists integer Insertion

---
+++ Lists - More Exercises/01. Distinct List/DistinctList.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.Distinct_List
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DistinctList
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' })
+                .Select(int.Parse)
+                .ToList()
+                .Distinct();
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
---
+++ Lists - More Exercises/02. Integer Insertion/IntegerInsertion.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.Integer_Insertion
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class IntegerInsertion
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' })
+                .Select(int.Parse)
+                .ToList();
+
+            var stringInput = Console.ReadLine();
+            numbers = Insertion(numbers, stringInput);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static List<int> Insertion(List<int> numbers, string stringInput)
+        {
+            while (stringInput != "end")
+            {
+                var insertIndex = int.Parse(stringInput[0].ToString());
+                var number = int.Parse(stringInput);
+
+                numbers.Insert(insertIndex, number);
+
+                stringInput = Console.ReadLine();
+            }
+
+            return numbers;
+        }
+    }
+}
