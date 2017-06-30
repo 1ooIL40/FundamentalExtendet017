b27ec5aa48fbbf48274ed084a0efd3095bd4f46b
1ooiL40 <valkovstoil@abv.bg>
2017-06-30 20:27:02 +0300

    Shoot List Elements Average Character Delimiter

---
+++ Array and List Algorithms - Exercises/02. Average Character Delimiter/AverageCharacterDelimiter.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.Average_Character_Delimiter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class AverageCharacterDelimiter
+    {
+        public static void Main()
+        {
+            string[] letters = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            int sumOfAllChars = 0;
+            int allChars = 0;
+
+
+            foreach (var letter in letters)
+            {
+                letter.ToCharArray();
+
+                foreach (var let in letter)
+                {
+                    sumOfAllChars += (int)let;
+                    allChars++;
+                }
+            }
+
+            char delimeter = (char)(sumOfAllChars / allChars);
+
+            Console.WriteLine(string.Join((delimeter
+                .ToString()
+                .ToUpper()),letters));
+        }
+    }
+}
