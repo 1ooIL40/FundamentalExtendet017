ac9b6282d31669d279efa5838e305e865165a9d8
1ooiL40 <valkovstoil@abv.bg>
2017-07-15 17:32:44 +0300

    Default Values Shopping Spree

---
+++ Lambda and LINQ - Exercises/02. Default Values/DefaultValues.cs
@@ -0,0 +1,52 @@
+﻿namespace _02.Default_Values
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DefaultValues
+    {
+        public static void Main()
+        {
+            Dictionary<string, string> deffaultValues = new Dictionary<string, string>();
+
+            string[] inputLine = Console.ReadLine()
+                .Split(new string[] { " -> " },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            while (inputLine[0] != "end")
+            {
+                string key = inputLine[0];
+                string value = inputLine[1];
+
+                
+                deffaultValues[key] = value;
+
+                inputLine = Console.ReadLine()
+                .Split(new string[] { " -> " },
+                StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string replacmentValue = Console.ReadLine();
+
+            Dictionary<string, string> originalValues = deffaultValues
+                .Where(v => v.Value != "null")
+                .OrderByDescending(v => v.Value.Length)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            Dictionary<string, string> nullValues = deffaultValues
+                .Where(v => v.Value == "null")
+                .ToDictionary(k => k.Key, v => replacmentValue);
+
+
+            Dictionary<string, string> result = originalValues
+                .Concat(nullValues)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} <-> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ Lambda and LINQ - Exercises/03. Shopping Spree/ShoppingSpree.cs
@@ -0,0 +1,63 @@
+﻿namespace _03.Shopping_Spree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ShoppingSpree
+    {
+        public static void Main()
+        {
+            Dictionary<string, decimal> products = new Dictionary<string, decimal>();
+
+            decimal budget = decimal.Parse(Console.ReadLine());
+
+            string[] inputLine = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            while (inputLine[0] != "end")
+            {
+
+                string product = inputLine[0];
+                decimal productPrice = decimal.Parse(inputLine[1]);
+
+                if (!products.ContainsKey(product))
+                {
+                    products[product] = productPrice;
+                }
+                else
+                {
+                    if(products[product] > productPrice)
+                    {
+                        products[product] = productPrice;
+                    }
+                }
+
+                inputLine = Console.ReadLine()
+               .Split(new char[] { ' ' },
+               StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            decimal sum = products.Values.Sum();
+
+
+            if(sum > budget)
+            {
+                Console.WriteLine("Need more money... Just buy banichka");
+            }
+            else
+            {
+                products = products
+                    .OrderByDescending(v => v.Value)
+                    .ThenBy(k => k.Key.Length)
+                    .ToDictionary(k => k.Key, v => v.Value);
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine($"{product.Key} costs {product.Value:f2}");
+                }
+            }
+        }
+    }
+}
