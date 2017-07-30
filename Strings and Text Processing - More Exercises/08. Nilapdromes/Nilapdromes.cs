cd57fb81e905249039590097bfd87f6b6c83ed58
Stoil Valkov <valkovstoil@abv.bg>
2017-07-30 17:44:41 +0300

    Nilapdromes working fine

--- Strings and Text Processing - More Exercises/08. Nilapdromes/Nilapdromes.cs
+++ Strings and Text Processing - More Exercises/08. Nilapdromes/Nilapdromes.cs
@@ -6,47 +6,91 @@
 
     public class Nilapdromes
     {
+
         public static void Main()
         {
             string nilapdromes = Console.ReadLine();
 
             while (nilapdromes != "end")
             {
-                string border = string.Empty;
+                string startBorder = string.Empty;
+                string endBorder = string.Empty;
                 string core = string.Empty;
 
-                char lastChar = nilapdromes.Last();
-                int subStringIndex = nilapdromes.Length - 1;
-                string substringToChek = nilapdromes.Substring(0,subStringIndex);
+                int len = nilapdromes.Length;
+                bool compare = false;
 
-                bool contain = substringToChek.Contains(lastChar);
-
-
-                //TODO logic for donomonodono input
-                if (contain)
+                int endBorderLenght = (len) - (len / 2);
+                startBorder = nilapdromes.Substring(0, len / 2);
+                if (len % 2 == 0)
                 {
-                    int index = substringToChek.IndexOf(lastChar);
+                    endBorder = nilapdromes.Substring(startBorder.Length, endBorderLenght);
+                }
+                else
+                {
+                    endBorderLenght = startBorder.Length;
+                    endBorder = nilapdromes.Substring(startBorder.Length + 1, endBorderLenght);
+                }
+                compare = startBorder.Equals(endBorder);
 
-                    border = nilapdromes.Substring(0, index + 1);
+                while (startBorder.Length > 0 && !compare)
+                {
+                    startBorder = NewStartBorder(startBorder);
+                    endBorder = NewEndBorder(endBorder);
 
-                    nilapdromes = nilapdromes.Replace(border, "#");
-
-                    if(nilapdromes.Last() == '#' && nilapdromes.Length > 1) 
+                    if (startBorder != "")
                     {
-                        core = nilapdromes.Replace("#", "").Trim();
+                        compare = startBorder.Equals(endBorder);
                     }
-
                 }
 
-
-                if(border != string.Empty && core != string.Empty)
+                if (compare)
                 {
-                    Console.WriteLine($"{core}{border}{core}");
-                }
+                    core = Core(nilapdromes, startBorder);
 
+                    if (core != "")
+                    {
+                        Console.WriteLine($"{core}{startBorder}{core}");
+                    }
+                }
 
                 nilapdromes = Console.ReadLine();
             }
+        }
+
+        static string Core(string nilapdromes, string startBorder)
+        {
+            string result = string.Empty;
+            int len = startBorder.Length;
+
+            for (int i = len; i < nilapdromes.Length - len; i++)
+            {
+                result += nilapdromes[i];
+            }
+
+            return result;
+        }
+
+        static string NewStartBorder(string startBorder)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < startBorder.Length - 1; i++)
+            {
+                result += startBorder[i];
+            }
+            return result;
+        }
+
+        static string NewEndBorder(string endBorder)
+        {
+            string result = string.Empty;
+
+            for (int i = 1; i <= endBorder.Length - 1; i++)
+            {
+                result += endBorder[i];
+            }
+            return result;
         }
     }
 }
