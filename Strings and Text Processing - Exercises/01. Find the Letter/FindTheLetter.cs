47919ae9091c9d70665ed386396b837dc2c74c44
Stoil Valkov <valkovstoil@abv.bg>
2017-07-30 20:41:29 +0300

    Find the Letter finished Points Counter at middle stage

--- Strings and Text Processing - Exercises/01. Find the Letter/FindTheLetter.cs
+++ Strings and Text Processing - Exercises/01. Find the Letter/FindTheLetter.cs
@@ -8,6 +8,29 @@
     {
         public static void Main()
         {
+            string inputString = Console.ReadLine();
+
+            string[] seoncLine = Console.ReadLine()
+                .Split(' ');
+            char finde = char.Parse(seoncLine[0]);
+            int index = int.Parse(seoncLine[1]);
+            int charIndex = inputString.IndexOf(finde, 0);
+
+            while (index != 1)
+            {
+                charIndex = inputString.IndexOf(finde, charIndex + 1);
+                
+                index--;
+            }
+           
+            if(charIndex > -1)
+            {
+                Console.WriteLine(charIndex);
+            }
+            else
+            {
+                Console.WriteLine("Find the letter yourself!");
+            }
         }
     }
 }
--- Strings and Text Processing - Exercises/05. Points Counter/PointsCounter.cs
+++ Strings and Text Processing - Exercises/05. Points Counter/PointsCounter.cs
@@ -8,7 +8,92 @@
     {
         public static void Main()
         {
+            string inputData = Console.ReadLine();
 
+            string prohibitedSymbols = "@%$*";
+            Dictionary<string,PlayerData> teams = new Dictionary<string, PlayerData>();
+
+            while (inputData != "Result")
+            {
+                string[] tokens = inputData
+                    .Split('|');
+
+                string name = FindeName(tokens,prohibitedSymbols);
+                string team = FindeTeam(tokens,prohibitedSymbols);
+
+                if (teams.ContainsKey(team))
+                {
+                    teams[team] = new PlayerData();
+                }
+                
+
+                int score = int.Parse(tokens[2]);
+
+                //TODO logic for add to dictionary and end result
+
+
+                inputData = Console.ReadLine();
+            }
         }
+
+        static string FindeTeam(string[] tokens, string prohibitedSymbols)
+        {
+            string first = tokens[0];
+            string second = tokens[1];
+
+            char[] badSymnols = prohibitedSymbols.ToCharArray();
+
+            first = RemoveBadSymbols(first, badSymnols);
+            second = RemoveBadSymbols(second, badSymnols);
+
+            char lastLetter = first.Last();
+
+            if (lastLetter > 64  && lastLetter < 91)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        static string RemoveBadSymbols(string word, char[] badSymnols)
+        {
+            for (int i = 0; i < badSymnols.Length; i++)
+            {
+                word = word.Replace(badSymnols[i].ToString(), "");
+            }
+
+            return word;
+        }
+
+        static string FindeName(string[] tokens, string prohibitedSymbols)
+        {
+            string first = tokens[0];
+            string second = tokens[1];
+
+            char[] badSymnols = prohibitedSymbols.ToCharArray();
+
+            first = RemoveBadSymbols(first, badSymnols);
+            second = RemoveBadSymbols(second, badSymnols);
+
+            char lastLetter = first.Last();
+
+            if (lastLetter > 64 && lastLetter < 91)
+            {
+                return second;
+            }
+            else
+            {
+                return first;
+            }
+        }
+    }
+
+    public class PlayerData
+    {
+        public string Name { get; set; }
+        public int Point { get; set; }
     }
 }
