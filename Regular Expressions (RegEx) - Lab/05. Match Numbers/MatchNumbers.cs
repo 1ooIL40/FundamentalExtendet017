53cfd91265950a481a2d2072f568b9d04ed4478e
Stoil Valkov <valkovstoil@abv.bg>
2017-08-07 20:35:48 +0300

    Match Numbers

---
+++ Regular Expressions (RegEx) - Lab/05. Match Numbers/MatchNumbers.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.Match_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class MatchNumbers
+    {
+        public static void Main()
+        {
+            Regex pattern = new Regex(@"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))");
+
+            string numberString = Console.ReadLine();
+
+            var numbers = pattern.Matches(numberString);
+
+            foreach (Match number in numbers)
+            {
+                Console.Write(number.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
