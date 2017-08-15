a80b9435d4ef71d810dcf9ac68b4259cd16c1876
Stoil Valkov <valkovstoil@abv.bg>
2017-08-15 21:02:42 +0300

    Rage Quit 100/100 Files 70/100 fix it

---
+++ Exam Preparation III - Taking a Sample Exam/03. Rage Quit/RageQuit.cs
@@ -0,0 +1,59 @@
+﻿namespace _03.Rage_Quit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class RageQuit
+    {
+        public static void Main()
+        {
+            string inputLine = Console.ReadLine().ToUpper();
+            string repeatingSymbols = Repeate(inputLine);
+            int uniqueSymols = UniqueSymbolsReturn(repeatingSymbols);
+
+            
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymols}");
+            Console.WriteLine(repeatingSymbols);
+        }
+
+        static string Repeate(string inputLine)
+        {
+            List<int> reapeateTimes = new List<int>();
+            Regex nums = new Regex(@"\d+");
+            var matches = nums.Matches(inputLine);
+            inputLine = nums.Replace(inputLine, "1");
+
+            string[] splitedSymbols = inputLine
+                .Split(new char[] { '1' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (Match match in matches)
+            {
+                reapeateTimes.Add(int.Parse(match.Value));
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < reapeateTimes.Count; i++)
+            {
+                string newString = string.Concat(Enumerable.Repeat(splitedSymbols[i], reapeateTimes[i]));
+                result.Append(newString);
+            }
+
+
+
+            return result.ToString();
+        }
+
+        static int UniqueSymbolsReturn(string syblos)
+        {
+            int result = syblos.Distinct().Count();
+
+            return result;
+        }
+    }
+}
---
+++ Exam Preparation III - Taking a Sample Exam/04. Files/Files.cs
@@ -0,0 +1,77 @@
+﻿namespace _04.Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    
+    public class Files
+    {
+        public static Dictionary<string,Dictionary<string,decimal>> dirAndFiles = new Dictionary<string,Dictionary<string,decimal>>();
+
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex pattertn = new Regex(@"^(?<path>[a-zA-Z].+\\)(?<file>.+)\;(?<size>[0-9]+)$");
+
+            for (int i = 0; i < n; i++)
+            {
+                string inputLine = Console.ReadLine();
+                var match = pattertn.Match(inputLine);
+
+                string path = match.Groups["path"].Value;
+                string fileAndExtention = match.Groups["file"].Value;
+                decimal size = decimal.Parse(match.Groups["size"].Value);
+
+                if (!dirAndFiles.ContainsKey(path))
+                {
+                    dirAndFiles[path] = new Dictionary<string, decimal>();
+                }
+                if (!dirAndFiles[path].ContainsKey(fileAndExtention))
+                {
+                    dirAndFiles[path][fileAndExtention] = 0;
+                }
+                dirAndFiles[path][fileAndExtention] = size;
+            }
+            string[] tokens = Console.ReadLine()
+                .Split(new char[] { ' ' },
+                 StringSplitOptions.RemoveEmptyEntries);
+
+            string root = tokens[2];
+            string file = tokens[0];
+
+            Dictionary<string, decimal> result = new Dictionary<string, decimal>();
+
+            foreach (var path in dirAndFiles)
+            {
+                if (path.Key.Contains(root))
+                {
+                    foreach (var files in path.Value)
+                    {
+                        result[files.Key] = files.Value;
+                    }
+                }
+            }
+
+            result = result
+                .OrderByDescending(v=>v.Value)
+                .ThenBy(k=>k.Key)
+                .Where(k => k.Key.Contains(file))
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            if(result.Count != 0)
+            {
+                foreach (var output in result)
+                {
+                    Console.WriteLine($"{output.Key} - {output.Value} KB ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            
+        }
+    }
+}
